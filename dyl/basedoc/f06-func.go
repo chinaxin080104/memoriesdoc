312166a649fe21ee06772757b01a6ec2621e1aac
dyl <735449187@qq.com>
2024-09-09 11:50:18 +0800

    update+1

--- dyl/basedoc/f06-func.go
+++ dyl/basedoc/f06-func.go
@@ -38,19 +38,11 @@ func main() {
 	// loginv2()
 
 	//函数的闭包规则
-	s := awaitAdd(2)(1, 2, 3)
-	fmt.Println(s)
-}
+	// s := awaitAdd(2)(1, 2, 3)
+	// fmt.Println(s)
 
-func awaitAdd(t int) func(...int) int {
-	time.Sleep(time.Duration(t) * time.Second)
-	return func(i ...int) int {
-		var sum int
-		for _, j := range i {
-			sum += j
-		}
-		return sum
-	}
+	//值传递与引用传递
+	funcyinyong()
 }
 
 func sayWelcome() {
@@ -159,4 +151,43 @@ func loginv2() {
 		3: logout,
 	}
 	funcMap[num]()
+}
+
+// 函数的闭包
+func awaitAdd(t int) func(...int) int {
+	time.Sleep(time.Duration(t) * time.Second)
+	//这里return 返回函数是因为返回类型里面约束了必须是函数。
+	return func(i ...int) int {
+		var sum int
+		// _ 表示不接收i 返回的key
+		for _, j := range i {
+			sum += j
+		}
+		return sum
+	}
+}
+
+func yingyong1(num int) {
+	fmt.Print(&num) //输出year 的物理地址--// 0xc00000a0882024
+	num = 2000
+	//值传递，在函数体内，不管如何修改值，都不会影响外面的值。
+
+}
+func yingyong2(num *int) {
+	//引用传递，我们需要直接将变量的内存地址传递进去。eg:2
+	// &是取地址，*是解引用，去这个地址指向的值
+	fmt.Println(num) //内存值
+	*num = 2025      //这里的修改会影响外面的num
+}
+
+func funcyinyong() {
+	year := 2024
+
+	fmt.Println(&year) // 0xc00000a080
+	yingyong1(year)
+	fmt.Println(year)
+
+	//引用传递
+	yingyong2(&year)
+	fmt.Println(year)
 }
