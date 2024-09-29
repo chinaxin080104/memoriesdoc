fdf6e2bf90516c7e45e085dcaa88ac5ce7bc60a5
dyl <735449187@qq.com>
2024-09-29 15:21:47 +0800

    update-2

--- dyl/gozero/api/sql/v1/model/user.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE user
-(
-    id        bigint AUTO_INCREMENT,
-    username  varchar(36) default '',
-    password  varchar(64) default '',
-    UNIQUE name_index (username),
-    PRIMARY KEY (id)
-) ENGINE = InnoDB COLLATE utf8mb4_general_ci;
